d7b5fdfe6b8a3ec85059c315ff011ee3fea9a9fa
Chrisphyne <31749898+Chrisphyne@users.noreply.github.com>
2020-12-22 14:30:31 +0300

    My first commit

---
+++ src/components/PostForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class PostForm extends Component {
+
+    
+
+
+    render() {
+        
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default PostForm;
